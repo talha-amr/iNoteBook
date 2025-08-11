ff8e452a1cba588f9a203bba35fb15a7b46348d1
Muhammad Talha Amir <tellyamir16@gmail.com>
2025-08-11 16:16:00 +0500

    Setup backend server with Express and MongoDB connection

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const connectToMongo=require('./db');
+const express = require('express')
+const cors = require('cors');
+connectToMongo();
+
+const app = express()
+app.use(cors());
+const port = 5000
+app.use(express.json());
+//routes that we are using
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`iNoteBook Backend listening on port ${port}`)
+})
