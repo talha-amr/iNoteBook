b3e9a1f9826edeecc346f2f418e57596e12d159a
Muhammad Talha Amir <tellyamir16@gmail.com>
2025-08-12 15:04:23 +0500

    Remove node_modules and backend from tracking

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
-
-const connectToMongo = () => {
-  mongoose.connect(mongoURI)
-    .then(() => console.log("✅ Connected to MongoDB"))
-    .catch((e) => console.log("❌ Connection error:", e.message));
-};
-module.exports = connectToMongo;
